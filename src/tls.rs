98d0a39f0eb969dcd2e8729309bbf91e082f6700
Mozk Taberenai <mozk@me.com>
2023-09-23 15:00:19 +0900

    tls::Error: add source and fix messages

--- src/tls.rs
+++ src/tls.rs
@@ -4,40 +4,62 @@ use tracing::info;
 
 #[derive(Debug)]
 pub enum Error {
-    NoCertificate(PathBuf),
-    NoPrivateKey(PathBuf),
-    Io(std::io::Error, PathBuf),
-    Rustls(rustls::Error),
+    NoCertificate {
+        path: PathBuf,
+    },
+    NoPrivateKey {
+        path: PathBuf,
+    },
+    Io {
+        source: std::io::Error,
+        path: PathBuf,
+    },
+    Rustls {
+        source: rustls::Error,
+    },
 }
 
 impl From<rustls::Error> for Error {
-    fn from(err: rustls::Error) -> Self {
-        Self::Rustls(err)
+    fn from(source: rustls::Error) -> Self {
+        Self::Rustls { source }
     }
 }
 
 impl std::fmt::Display for Error {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         match self {
-            Error::NoCertificate(path) => write!(f, "no certificate: {}", path.display()),
-            Error::NoPrivateKey(path) => write!(f, "no private key: {}", path.display()),
-            Error::Io(source, path) => write!(f, "{}: {source:?}", path.display()),
-            Error::Rustls(err) => write!(f, "{err:?}"),
+            Error::NoCertificate { path } => write!(f, "No certificates in {}", path.display()),
+            Error::NoPrivateKey { path } => write!(f, "No private key in {}", path.display()),
+            Error::Io { path, .. } => write!(f, "IO error on {}", path.display()),
+            Error::Rustls { .. } => write!(f, "Rustls error"),
         }
     }
 }
 
-impl std::error::Error for Error {}
+impl std::error::Error for Error {
+    fn source(&self) -> Option<&(dyn std::error::Error + 'static)> {
+        match self {
+            Error::NoCertificate { .. } => None,
+            Error::NoPrivateKey { .. } => None,
+            Error::Io { source, .. } => Some(source),
+            Error::Rustls { source } => Some(source),
+        }
+    }
+}
 
 pub fn acceptor(pem: impl AsRef<Path>) -> Result<tokio_rustls::TlsAcceptor, Error> {
     let pem = pem.as_ref();
-    let pem_buf = std::fs::read(pem).map_err(|source| Error::Io(source, pem.to_owned()))?;
+    let pem_buf = std::fs::read(pem).map_err(|source| Error::Io {
+        source,
+        path: pem.to_owned(),
+    })?;
 
     let mut certs = vec![];
     let mut key = None;
-    for item in rustls_pemfile::read_all(&mut &pem_buf[..])
-        .map_err(|source| Error::Io(source, pem.to_owned()))?
-    {
+    for item in rustls_pemfile::read_all(&mut &pem_buf[..]).map_err(|source| Error::Io {
+        source,
+        path: pem.to_owned(),
+    })? {
         match item {
             rustls_pemfile::Item::X509Certificate(buf) => {
                 info!(pem=%pem.display(), "found x509 certificate");
@@ -60,10 +82,14 @@ pub fn acceptor(pem: impl AsRef<Path>) -> Result<tokio_rustls::TlsAcceptor, Erro
     }
 
     if certs.is_empty() {
-        return Err(Error::NoCertificate(pem.to_owned()));
+        return Err(Error::NoCertificate {
+            path: pem.to_owned(),
+        });
     }
     if key.is_none() {
-        return Err(Error::NoPrivateKey(pem.to_owned()));
+        return Err(Error::NoPrivateKey {
+            path: pem.to_owned(),
+        });
     }
     let key = key.unwrap();
 
